8d183792a44b4772c165278be24503d2399f2a28
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-06-06 06:12:10 +0100

    add the Team's page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,17 @@ const Header = ({title, subtitle}) => {
 	const colors = tokens(theme.palette.mode);
 	return (
 		<Box mb="30px">
-			<Typography variant="h2" color={colors.grey[100]} sx={{ mb: "5px" }}>
+			<Typography
+				variant="h2"
+				color={colors.grey[100]}
+				fontWeight="bold"
+				sx={{ m: "0 0 5px 0" }}
+			>
 				{title}
 			</Typography>
-			<Typography variant="h5" color={colors.greenAccent[400]}>{subtitle}</Typography>
+			<Typography variant="h5" color={colors.greenAccent[400]}>
+				{subtitle}
+			</Typography>
 		</Box>
 	);
 };
